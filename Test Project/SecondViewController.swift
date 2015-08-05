438050c41e0a5fd5667e716f822c53811d78e6a3
Calan Cicolini <38863@cgs.act.edu.au>
2015-08-05 11:22:10 +1000

    New ViewController

---
+++ Test Project/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  Test Project
+//
+//  Created by Calan Cicolini on 5/08/2015.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+            
+            // Debug line of code
+            println("Hello World")
+        }
+}
+
+
